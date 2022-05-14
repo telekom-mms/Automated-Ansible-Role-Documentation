c419c32eb35222ffe48c80e869c34d3676a675a5
Miika Kankare <miika@kanka.re>
2022-05-14 19:35:30 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,135 @@
+#!/usr/bin/env python3
+
+import textwrap
+import yaml
+import click
+
+from pathlib import Path
+from jinja2 import Environment
+
+
+@click.group()
+def cli():
+    pass
+
+
+@cli.command()
+@click.argument(
+    "role",
+    type=click.Path(
+        exists=True, file_okay=False, readable=True, writable=True, resolve_path=True
+    ),
+)
+@click.option(
+    "--output-file",
+    help="The output file",
+    default="README.md",
+    type=click.File("w+", lazy=True),
+)
+@click.option(
+    "--output-template",
+    help="The output template string.",
+    default="<!-- BEGIN_ANSIBLE_DOCS -->\n{{ content }}\n<!-- END_ANSIBLE_DOCS -->\n",
+    type=click.STRING,
+)
+def markdown(role: str, output_file: click.File, output_template: str) -> None:
+    env = Environment()
+    template = env.from_string(source=output_template.replace("\\n", "\n"))
+
+    metadata, argument_specs = parse_meta(role)
+    content = generate_content(role, metadata, argument_specs)
+
+    output_file.write(
+        template.render(
+            content=content, role=role, metadata=metadata, argument_specs=argument_specs
+        )
+    )
+
+
+def parse_meta(role: str) -> tuple[dict, dict]:
+    meta = Path(f"{role}/meta")
+
+    argument_specs_yml = list(meta.glob("argument_specs.y*ml"))[0]
+    main_yml = list(meta.glob("main.y*ml"))[0]
+
+    with open(argument_specs_yml, "r") as f:
+        argument_specs = yaml.safe_load(f)
+        argument_specs = argument_specs["argument_specs"]
+
+    with open(main_yml, "r") as f:
+        main = yaml.safe_load(f)
+
+    return main, argument_specs
+
+
+def generate_content(role: str, metadata: dict, argument_specs: dict) -> str:
+    content = """
+    Ansible Role: {{ role | capitalize }}
+    =========
+
+    {{ metadata.galaxy_info.description }}
+
+    Tags: {{ metadata.galaxy_info.galaxy_tags | join(', ') }}
+
+    Requirements
+    ------------
+
+    | Platform | Versions |
+    | -------- | -------- |
+    {%- for platform in metadata.galaxy_info.platforms %}
+    | {{ platform.name }} | {{ platform.versions | join(', ') }} |
+    {%- endfor %}
+
+    Role Variables
+    --------------
+
+    {% for entrypoint, specs in argument_specs | items -%}
+    ## {{ entrypoint }}
+
+    {{ specs.short_description }}
+
+    | Variable | Description | Type | Required | Default |
+    | -------- | ----------- | ---- | -------- | ------- |
+    {%- for name, variable in specs.options | items %}
+    | {{ name }} | {{ variable.description }} | {{ variable.type }} | {{ 'yes' if variable.required else 'no' }} | {{ variable.default }} |
+    {%- endfor %}
+    {% endfor %}
+
+    Dependencies
+    ------------
+
+    {% for dependency in metadata.dependencies -%}
+    - {{ dependency }}
+    {% endfor %}
+
+    Example Playbook
+    ----------------
+
+    Including an example of how to use your role (for instance, with variables passed in as parameters) is always nice for users too:
+
+        - hosts: servers
+          roles:
+             - { role: username.rolename, x: 42 }
+
+    License
+    -------
+
+    {{ metadata.galaxy_info.license }}
+
+    Author Information
+    ------------------
+
+    {{ metadata.galaxy_info.author }} @ {{ metadata.galaxy_info.company }}
+
+    Issues: {{ metadata.galaxy_info.issue_tracker_url }}
+
+    """
+
+    env = Environment()
+    template = env.from_string(source=textwrap.dedent(content.lstrip("\n")))
+
+    return template.render(metadata=metadata, argument_specs=argument_specs)
+
+
+if __name__ == "__main__":
+    cli()
