90ba1c6addb8cba71d9d71fffbefefe8913171c8
Miika Kankare <miika@kanka.re>
2022-06-18 09:45:36 +0300

    Add configuration file option

--- ansible_docs/cli.py
+++ ansible_docs/cli.py
@@ -8,8 +8,7 @@ import jinja2
 import typer
 import yaml
 
-app = typer.Typer()
-state = {}
+app = typer.Typer(no_args_is_help=True)
 
 
 class OutputMode(str, Enum):
@@ -17,18 +16,47 @@ class OutputMode(str, Enum):
     replace = "replace"
 
 
+def parse_config(
+    ctx: typer.Context, param: typer.CallbackParam, config_file: pathlib.Path
+) -> pathlib.Path:
+    # FIXME: This will change the defaults when called with --help
+    #
+    # See: https://github.com/tiangolo/typer/issues/347
+    if config_file.exists():
+        try:
+            with open(config_file, "r") as f:
+                content = yaml.safe_load(f)
+
+                ctx.default_map = ctx.default_map or {}
+                ctx.default_map.update(content)
+        except Exception as ex:
+            raise typer.BadParameter(str(ex))
+
+    return config_file
+
+
 @app.command()
-def markdown() -> None:
+def markdown(ctx: typer.Context) -> None:
     """
     Command for generating role documentation in Markdown format.
     """
 
-    content = render_content("markdown.j2")
-    write(content)
+    # FIXME: ctx should not be passed to these non-command()'s
+    content = render_content(ctx, "markdown.j2")
+    write(ctx, content)
 
 
 @app.callback()
 def cli(
+    ctx: typer.Context,
+    config_file: pathlib.Path = typer.Option(
+        ".ansible-docs.yml",
+        file_okay=True,
+        dir_okay=False,
+        readable=True,
+        is_eager=True,
+        callback=parse_config,
+    ),
     role_path: pathlib.Path = typer.Argument(
         ...,
         help="Path to an Ansible role",
@@ -39,46 +67,54 @@ def cli(
         writable=True,
         resolve_path=True,
     ),
-    output_file: str = typer.Option(
+    output_file: pathlib.Path = typer.Option(
         "README.md",
         file_okay=True,
         dir_okay=False,
         readable=True,
         writable=True,
     ),
-    output_template: str = "<!-- BEGIN_ANSIBLE_DOCS -->\n{{ content }}\n<!-- END_ANSIBLE_DOCS -->\n\n",
+    output_template: str = "<!-- BEGIN_ANSIBLE_DOCS -->\n{{ content }}\n<!-- END_ANSIBLE_DOCS -->\n",
     output_mode: OutputMode = typer.Option(OutputMode.inject.name),
 ):
     """
     A tool for generating docs for Ansible roles.
     """
 
-    state["role"] = role_path.stem
-    state["role_path"] = role_path
-    state["output_file"] = output_file
-    state["output_template"] = output_template
-    state["output_mode"] = output_mode
+    ctx.ensure_object(dict)
 
-    state["metadata"], state["argument_specs"] = parse_meta()
+    ctx.obj["data"] = {}
+    ctx.obj["config"] = {}
+
+    ctx.obj["config"]["role"] = role_path.stem
+    ctx.obj["config"]["role_path"] = role_path
+    ctx.obj["config"]["output_file"] = output_file
+    ctx.obj["config"]["output_template"] = output_template
+    ctx.obj["config"]["output_mode"] = output_mode
+
+    (
+        ctx.obj["data"]["metadata"],
+        ctx.obj["data"]["argument_specs"],
+    ) = parse_meta(ctx)
 
 
-def write(content: str) -> None:
+def write(ctx: typer.Context, content: str) -> None:
     """
     Writes a content string to the given file.
     """
 
-    output = pathlib.Path(state["output_file"])
+    output = ctx.obj["config"]["output_file"]
 
-    if not "/" in state["output_file"]:
-        output = state["role_path"] / output
+    if not "/" in str(output):
+        output = ctx.obj["config"]["role_path"] / output
 
     output.resolve()
 
     if not output.exists():
-        state["output_mode"] = OutputMode.replace
+        ctx.obj["config"]["output_mode"] = OutputMode.replace
 
     with open(output, "a+") as f:
-        if state["output_mode"] == OutputMode.inject:
+        if ctx.obj["config"]["output_mode"] == OutputMode.inject:
             f.seek(0)
             lines = f.readlines()
 
@@ -94,12 +130,12 @@ def write(content: str) -> None:
         f.write(content)
 
 
-def parse_meta() -> tuple[dict, dict]:
+def parse_meta(ctx: typer.Context) -> tuple[dict, dict]:
     """
     Parses Ansible role metadata from YAML files in the meta/ directory.
     """
 
-    meta = state["role_path"] / "meta"
+    meta = ctx.obj["config"]["role_path"] / "meta"
 
     argument_specs_yml = list(meta.glob("argument_specs.y*ml"))[0]
     main_yml = list(meta.glob("main.y*ml"))[0]
@@ -114,7 +150,7 @@ def parse_meta() -> tuple[dict, dict]:
     return main, argument_specs
 
 
-def render_content(content_template: str) -> str:
+def render_content(ctx: typer.Context, content_template: str) -> str:
     """
     Renders Jinja2 templates twice, the first time to get the parsed
     data into {{ content }} and finally to render the user provided
@@ -126,19 +162,21 @@ def render_content(content_template: str) -> str:
 
     env = jinja2.Environment(loader=loader)
     content = env.get_template(content_template).render(
-        role=state["role"],
-        metadata=state["metadata"],
-        argument_specs=state["argument_specs"],
+        role=ctx.obj["config"]["role"],
+        metadata=ctx.obj["data"]["metadata"],
+        argument_specs=ctx.obj["data"]["argument_specs"],
     )
 
     env = jinja2.Environment()
-    template = env.from_string(source=state["output_template"].replace("\\n", "\n"))
+    template = env.from_string(
+        source=ctx.obj["config"]["output_template"].replace("\\n", "\n")
+    )
 
     return template.render(
         content=content,
-        role=state["role"],
-        metadata=state["metadata"],
-        argument_specs=state["argument_specs"],
+        role=ctx.obj["config"]["role"],
+        metadata=ctx.obj["data"]["metadata"],
+        argument_specs=ctx.obj["data"]["argument_specs"],
     )
 
 
